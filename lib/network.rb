d19a49c26fe71ddc71e8b1d02635a08a98064f0f
Weston Ellis <ellisweston112@gmail.com>
2021-07-21 10:19:38 -0600

    Finish iteration 4

--- lib/network.rb
+++ lib/network.rb
@@ -10,11 +10,13 @@ class Network
     @shows << show
   end
 
-  def main_character
-    all_characters = @shows.flat_map do |show|
+  def all_characters
+    @shows.flat_map do |show|
       show.characters
     end.uniq
+  end
 
+  def main_character
     all_characters.find_all do |character|
       character.salary > 500_000 && character.name.upcase == character.name
     end
@@ -26,5 +28,30 @@ class Network
       actors_show[show] = show.actors
     end
     actors_show
+  end
+
+  def find_show_by_actor(actor)
+    @shows.find_all do |show|
+      show.actors.include?(actor)
+    end
+
+  end
+
+  def show_by_actor
+    actor_show_hash = {}
+    all_characters.each do |character|
+      actor_show_hash[character.actor] = find_show_by_actor(character.actor)
+    end
+    actor_show_hash
+  end
+
+  def prolific_actors
+    prolific = []
+    show_by_actor.each do |actor, shows|
+      if shows.length >1
+        prolific << actor
+      end
+    end
+    prolific
   end
 end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -38,12 +38,38 @@ RSpec.describe do
     ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
     parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
     mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
 
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    nbc.add_show(baywatch)
 
+    expect(nbc.show_by_actor).to eq({
+      "David Hasselhoff" => [knight_rider, baywatch],
+      "William Daniels" => [knight_rider],
+      "Amy Poehler" => [parks_and_rec],
+      "Nick Offerman" => [parks_and_rec]
+      })
+
+    expect(nbc.prolific_actors).to eq(["David Hasselhoff"])
 
   end
 
+  it 'can find a show by actor' do
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
 
+    nbc.add_show(parks_and_rec)
+
+    expect(nbc.find_show_by_actor("Nick Offerman")).to eq([parks_and_rec])
+  end
 
 
 end
