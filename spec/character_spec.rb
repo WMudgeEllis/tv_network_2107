45dbc2cd7e8036023696e9070c5f34e84fae34b6
Weston Ellis <ellisweston112@gmail.com>
2021-07-21 09:07:49 -0600

    Finish iteration 1

---
+++ spec/character_spec.rb
@@ -0,0 +1,20 @@
+require './lib/character'
+
+
+
+RSpec.describe do
+
+  it 'exists and can fetch name, actor, and salary' do
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    expect(kitt.name).to eq('KITT')
+    expect(kitt.actor).to eq('William Daniels')
+    expect(kitt.salary).to eq(1000000)
+  end
+
+
+
+
+
+
+end
