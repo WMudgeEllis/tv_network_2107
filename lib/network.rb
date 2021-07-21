fdbdebf5b75c575ab2271dfea6a5ca1e7535ce97
Weston Ellis <ellisweston112@gmail.com>
2021-07-21 09:49:35 -0600

    Finish iteration 3 for reals this time

--- lib/network.rb
+++ lib/network.rb
@@ -10,12 +10,21 @@ class Network
     @shows << show
   end
 
+  def main_character
+    all_characters = @shows.flat_map do |show|
+      show.characters
+    end.uniq
+
+    all_characters.find_all do |character|
+      character.salary > 500_000 && character.name.upcase == character.name
+    end
+  end
+
   def actors_by_show
     actors_show = {}
     @shows.each do |show|
       actors_show[show] = show.actors
     end
     actors_show
-
   end
 end
--- spec/network_spec.rb
+++ spec/network_spec.rb
@@ -22,12 +22,26 @@ RSpec.describe do
     nbc.add_show(knight_rider)
     nbc.add_show(parks_and_rec)
     expect(nbc.shows).to eq([knight_rider, parks_and_rec])
+    expect(nbc.main_character).to eq([kitt])
     expect(nbc.actors_by_show).to eq({
       knight_rider => ["David Hasselhoff", "William Daniels"],
       parks_and_rec => ["Amy Poehler", "Nick Offerman"]
       })
   end
 
+  it 'can pass iteration 4' do
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+
+
+
+  end
 
 
 
